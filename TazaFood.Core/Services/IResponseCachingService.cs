ad2f926e50ce72c09e2d8e414bf41e09bab0b240
Your Ahmed <ah1942892@gmail.com>
2023-12-15 11:01:59 +0200

    Caching Service

---
+++ TazaFood.Core/Services/IResponseCachingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TazaFood.Core.Services
+{
+    public interface IResponseCachingService
+    {
+        Task CachingResponseAsync(string cacheKey, object response, TimeSpan timeToLive);
+        Task<string> GetCachedResponse(string cacheKey);
+    }
+}
---
+++ TazaFood.Service/CacheService/ResponseCacheService.cs
@@ -0,0 +1,41 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TazaFood.Core.Services;
+
+namespace TazaFood.Service.CacheService
+{
+    public class ResponseCacheService : IResponseCachingService
+    {
+        private readonly IDatabase _database;
+        public ResponseCacheService(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+        public async Task CachingResponseAsync(string cacheKey, object response, TimeSpan timeToLive)
+        {
+            if (response == null) return;
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            };
+
+            var serializedResponse = JsonSerializer.Serialize(response, options);
+            await _database.StringSetAsync(cacheKey, serializedResponse , timeToLive);
+        }
+
+        public async Task<string> GetCachedResponse(string cacheKey)
+        {
+            var cachedResponse = await _database.StringGetAsync(cacheKey);
+            if (cachedResponse.IsNullOrEmpty)
+            {
+                return null;
+            }
+            return cachedResponse;
+        }
+    }
+}

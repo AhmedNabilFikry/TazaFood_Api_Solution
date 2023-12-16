2f4b752c071126f01f19a8fbee02fe6d2d6b153d
Your Ahmed <ah1942892@gmail.com>
2023-12-16 22:54:37 +0200

    Crud For Category

--- TazaFood_Api/Controllers/CategoryController.cs
+++ TazaFood_Api/Controllers/CategoryController.cs
@@ -10,11 +10,13 @@ namespace TazaFood_Api.Controllers
     {
         private readonly IGenericRepository<Category> _genericRepository;
         private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public CategoryController(IGenericRepository<Category> genericRepository, ICategoryRepository categoryRepository)
+        public CategoryController(IGenericRepository<Category> genericRepository, ICategoryRepository categoryRepository, IUnitOfWork unitOfWork )
         {
             _genericRepository = genericRepository;
             _categoryRepository = categoryRepository;
+            _unitOfWork = unitOfWork;
         }
 
         [HttpGet("GetCategories")]
@@ -38,6 +40,88 @@ namespace TazaFood_Api.Controllers
                 return NotFound("This Category is not Found");
             }
             return Ok(Category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category newCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Category category = new Category() 
+                    { 
+                        Name = newCategory.Name,
+                        Description = newCategory.Description
+                    };
+
+                    await _unitOfWork.Repository<Category>().Add(category);
+                    await _unitOfWork.Complete();
+                    return Ok("newCategory Added Successfully" + category);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Error in CreateCategory: {ex.Message}");
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Category>> UpdateCategory(int Id, Category updatedCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var category = await _unitOfWork.Repository<Category>().GetByIDAsync(Id);
+                    if (category == null)
+                    {
+                        return NotFound("Category not found.");
+                    }
+
+                    category.Name = updatedCategory.Name;
+                    category.Description = updatedCategory.Description;
+                    await _unitOfWork.Complete();
+
+                    return Ok("Updated Successfully");
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Error in UpdateCategory: {ex.Message}");
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int ID)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var existedCategory = await _unitOfWork.Repository<Category>().GetByIDAsync(ID);
+
+                    if (existedCategory == null)
+                    {
+                        return NotFound("Category with ID {categoryId} not found.");
+                    }
+
+                    _unitOfWork.Repository<Category>().Delete(existedCategory);
+                    await _unitOfWork.Complete();
+
+                    return Ok("Category deleted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"Error in DeleteCategory: {ex.Message}");
+                    return StatusCode(500, $"Internal server error: {ex.Message}");
+                }
+            }
+            return BadRequest(ModelState);
         }
     }
 }
